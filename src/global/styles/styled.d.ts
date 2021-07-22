7838478f6648355217ffcc0821a5fa8e05c7968d
Lucas Cezar <lmcezar@gmail.com>
2021-07-21 23:12:56 -0300

    style: Convertendo o card inicial para styled-components

---
+++ src/global/styles/styled.d.ts
@@ -0,0 +1,9 @@
+//import { DefaulTheme } from './styled.d';
+import 'styled-components';
+import theme from './theme';
+
+declare module 'styled-components' {
+  type ThemeType = typeof theme
+
+  export interface DefaultTheme extends ThemeType {}
+}
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+
+
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  UserGreeting,
+  User,
+  UserName,
+  Icon,
+  HighlightCards
+} from './styles';
+
+export function Dashboard() {
+  return (
+      <Container>
+        <Header>
+          <UserWrapper>
+            <UserInfo>
+              <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/3679709?v=4' }} />
+              <User>
+                <UserGreeting>Olá,</UserGreeting>
+                <UserName>Lucas</UserName>
+              </User>
+            </UserInfo>
+
+            <Icon name="power" />
+          </UserWrapper>
+
+        </Header>
+
+        <HighlightCards>
+          <HighlightCard />
+          <HighlightCard />
+          <HighlightCard />
+        </HighlightCards>
+      </Container>
+  )
+}
+
