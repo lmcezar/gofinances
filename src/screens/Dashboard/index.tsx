a067dc947fa85eab0ab709142ecb8fc469f8fa71
Lucas Cezar <lmcezar@gmail.com>
2021-09-05 22:53:34 -0300

    Finalizando a lista

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useState , useEffect} from 'react';
+import React, { useState , useEffect, useCallback} from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard, TransactionCardProps } from '../../components/TransacationCard';
 
@@ -63,7 +65,11 @@ export function Dashboard() {
   
   useEffect(() => {
     loadTransactions();
-  }, [])
+  }, []);
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions();
+  },[]));
   
 
 
